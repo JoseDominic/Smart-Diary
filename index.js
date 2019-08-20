e240003d2caff08565e0446fc04bde9e7dfd26a4
Jose Dominic <josudominic007@gmail.com>
2019-08-20 21:34:45 +0530

    8080

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var http = require('http');
+// Load the http module to create an http server.
+const http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Last, but not least, listen on port 8080
+// The environment variable PORT is automatically defined and equals to 8080
+server.listen(process.env.PORT, '0.0.0.0');
