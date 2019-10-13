cd0479eef94c40704dc16ecc1d0b36792dd54121
ARAVIND MS <55455165+Aravindms369@users.noreply.github.com>
2019-10-13 13:11:54 +0530

    prototype 1.0

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const {ensureAuthenticated} = require('../config/auth');
 //This is the main router from host
 
 //Welcome Page
-router.get('/',(req,res)=>res.render('welcome'));
+router.get('/',(req,res)=>res.render('welcome',{layout:false}));
 
 //User Dashboard
 
 //Pass in ensureAuthenticated object to all routes that need protection
 
-router.get('/dashboard',ensureAuthenticated,(req,res) => 
+router.get('/dashboard',ensureAuthenticated,(req,res) =>
     res.render('dashboard',{ //sending the name of user along with res to access username from dashboard
         name:req.user.name
     }));
